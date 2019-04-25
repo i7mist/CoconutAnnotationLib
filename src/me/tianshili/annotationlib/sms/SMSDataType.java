3e05000b5930354940ab4e30810661a167cdf48d
Tiffo Yu <tiffanyy9968@gmail.com>
2019-04-25 16:44:04 -0400

    added SMSDataType values

--- src/me/tianshili/annotationlib/sms/SMSDataType.java
+++ src/me/tianshili/annotationlib/sms/SMSDataType.java
@@ -5,4 +5,18 @@ package me.tianshili.annotationlib.sms;
  */
 
 public enum SMSDataType {
+    //BaseMmsColumns
+    LOCATION, //CONTENT_LOCATION
+    CREATOR, //CREATOR
+    DATE_AND_TIME, //DATE, DATE_SENT, DELIVERY_TIME
+    MESSAGE_ID, //MESSAGE_ID
+    STATUS, //STATUS, SEEN, RESPONSE_STATUS, SEEN_STATUS, READ_STATUS, READ
+    TEXT, //SUBJECT, RESPONSE_TEXT
+
+    //CanonicalAddressesColumns
+    ADDRESS, //ADDRESS (address used in MMS or SMS)
+
+    CONTENT_URI,
+
+    UNKNOWN
 }
