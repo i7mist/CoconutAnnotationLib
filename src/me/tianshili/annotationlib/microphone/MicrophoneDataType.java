6b05bcc80d85d2d6b4e506e816ab0d32809a89c4
elijah-neundorfer <elijah.neundorfer@gmail.com>
2019-06-03 15:26:25 -0400

    Updating Microphone Annotation to remove DataType

--- src/me/tianshili/annotationlib/microphone/MicrophoneDataType.java
+++
@@ -1,9 +0,0 @@
-package me.tianshili.annotationlib.microphone;
-
-/**
- * @author elijahneundorfer on 6/3/19
- */
-public enum MicrophoneDataType {
-    raw_data, //TODO (ELIJAH): This is a placeholder. Need to determine various types of data to place here.
-    UNKNOWN
-}
