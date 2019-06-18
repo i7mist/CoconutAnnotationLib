674a5184bc9fce5077365dc4a03cef21e49c890c
elijah-neundorfer <elijah.neundorfer@gmail.com>
2019-06-18 15:50:31 -0400

    Implementing Sensor Data

---
+++ src/me/tianshili/annotationlib/sensors/SensorAnnotation.java
@@ -0,0 +1,14 @@
+package me.tianshili.annotationlib.sensors;
+
+import me.tianshili.annotationlib.commons.Visibility;
+
+/**
+ * @author Elijah Neundorfer 6/17/19
+ * @version 6/17/19
+ */
+public interface SensorAnnotation {
+    SensorDataType[] dataType();
+    Visibility[] visibility();
+    SensorPurpose[] purpose();
+    String [] purposeDescription();
+}
---
+++ src/me/tianshili/annotationlib/sensors/SensorDataType.java
@@ -0,0 +1,9 @@
+package me.tianshili.annotationlib.sensors;
+
+/**
+ * @author Elijah Neundorfer 6/17/19
+ * @version 6/17/19
+ */
+public enum SensorDataType {
+    UNKNOWN,
+}
