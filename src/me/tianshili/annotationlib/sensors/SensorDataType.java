d950f594bc614e68d2fb04e058ecaa3e8bba172d
elijah-neundorfer <elijah.neundorfer@gmail.com>
2019-06-19 15:41:06 -0400

    Finalizing Base Sensor impl

--- src/me/tianshili/annotationlib/sensors/SensorDataType.java
+++ src/me/tianshili/annotationlib/sensors/SensorDataType.java
@@ -2,8 +2,9 @@ package me.tianshili.annotationlib.sensors;
 
 /**
  * @author Elijah Neundorfer 6/17/19
- * @version 6/17/19
+ * @version 6/19/19
  */
 public enum SensorDataType {
+    RAW_DATA,
     UNKNOWN,
 }
