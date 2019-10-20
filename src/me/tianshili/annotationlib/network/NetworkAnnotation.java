324302e917b25950263112c1380b6b5334ce9853
Tianshi Li <9413fi@gmail.com>
2019-10-19 21:54:40 -0400

    rename @Network to @NetworkAnnotation

--- src/me/tianshili/annotationlib/network/NetworkAnnotation.java
+++ src/me/tianshili/annotationlib/network/NetworkAnnotation.java
@@ -4,6 +4,6 @@ package me.tianshili.annotationlib.network;
  * Created by tianshi on 11/30/17.
  */
 
-public @interface Network {
+public @interface NetworkAnnotation {
     String [] destinations();
 }
