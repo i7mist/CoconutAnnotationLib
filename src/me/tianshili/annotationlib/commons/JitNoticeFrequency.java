7e298b2d41a13cdb50faed6f8d18af3803a8a377
Tianshi Li <9413fi@gmail.com>
2020-07-19 18:38:11 -0700

    change JitNoticeFrequency option

--- src/me/tianshili/annotationlib/commons/JitNoticeFrequency.java
+++ src/me/tianshili/annotationlib/commons/JitNoticeFrequency.java
@@ -1,9 +1,9 @@
 package me.tianshili.annotationlib.commons;
 
 public enum JitNoticeFrequency {
-    NEVER,
-    FIRST_TIME_ONLY,
-    PER_SESSION,
-    ALWAYS,
+    DO_NOT_SEND_NOTIFICATION,
+    SEND_NOTIFICATION_SILENTLY,
+    NOTIFICATION_POP_OUT_FIRST_TIME_ONLY,
+    NOTIFICATION_ALWAYS_POP_OUT,
     UNKNOWN
 }
