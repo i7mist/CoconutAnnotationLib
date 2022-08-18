08f2cb4ee7a98dfb02f78cf7869ffb90c0226b62
Tianshi Li <9413fi@gmail.com>
2022-08-17 23:37:52 -0700

    rename EndToEndEncryption to UserToUserEncryption

--- src/me/tianshili/annotationlib/collectionAttribute/EndToEndEncryption.java
+++
@@ -1,6 +0,0 @@
-package me.tianshili.annotationlib.collectionAttribute;
-
-public class EndToEndEncryption {
-    public static final String True = "(collection is exempt) EndToEndEncryption: true";
-    public static final String False = "EndToEndEncryption: false";
-}
---
+++ src/me/tianshili/annotationlib/collectionAttribute/UserToUserEncryption.java
@@ -0,0 +1,6 @@
+package me.tianshili.annotationlib.collectionAttribute;
+
+public class UserToUserEncryption {
+    public static final String True = "(collection is exempt) UserToUserEncryption: true";
+    public static final String False = "UserToUserEncryption: false";
+}
