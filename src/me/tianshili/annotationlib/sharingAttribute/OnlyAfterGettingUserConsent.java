efe54da5a6b359368b91e04f9e210be4c9f54f0e
Tianshi Li <9413fi@gmail.com>
2022-08-17 19:36:06 -0700

    reword some options

---
+++ src/me/tianshili/annotationlib/sharingAttribute/OnlyAfterGettingUserConsent.java
@@ -0,0 +1,6 @@
+package me.tianshili.annotationlib.sharingAttribute;
+
+public class OnlyAfterGettingUserConsent {
+    public static final String True = "(sharing is exempt) OnlyAfterGettingUserConsent: true";
+    public static final String False = "OnlyAfterGettingUserConsent: false";
+}
--- src/me/tianshili/annotationlib/sharingAttribute/OnlyAfterGettingUserConsentWithProminentDisclosure.java
+++
@@ -1,6 +0,0 @@
-package me.tianshili.annotationlib.sharingAttribute;
-
-public class OnlyAfterGettingUserConsentWithProminentDisclosure {
-    public static final String True = "(sharing is exempt) OnlyAfterGettingUserConsentWithProminentDisclosure: true";
-    public static final String False = "OnlyAfterGettingUserConsentWithProminentDisclosure: false";
-}
